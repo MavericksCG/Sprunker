3cf185f89f868984910298b9509ccf661d9ee7df
TheMaverick <89474607+MavericksCG@users.noreply.github.com>
2022-01-28 12:44:23 +0530

    Failed Miserably
    :(

--- Minimalistic Puzzle Platformer Game/Assets/Project/Scripts/Managing/GameManager.cs
+++ Minimalistic Puzzle Platformer Game/Assets/Project/Scripts/Managing/GameManager.cs
@@ -1,15 +1,23 @@
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
+    
     [Header("Assignables")]
+
     public GameObject lcUI;
     public GameObject goUI;
     public GameObject indi;
+    
     public GameObject player;
-
+    public GameObject env;
+    
     public static GameManager instance;
+
+    public Color bgColour;
+
+    [Range(0f, 1f)] [Tooltip("How fast the background colour should interpolate to the custom colour")] public float colorLerpSpeed;
 
 
     private void Awake () {
@@ -21,7 +29,6 @@ public class GameManager : MonoBehaviour
         QuickRestart();
     }
 
-    
     private void QuickRestart () {
 
         if (Input.GetKey(Keybinds.instance.quickRestart)) {
@@ -39,6 +46,8 @@ public class GameManager : MonoBehaviour
     }
 
     public void EndGame () {
+        Destroy(env, 0.5f);
+        Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor, bgColour, colorLerpSpeed * Time.deltaTime);
         goUI.SetActive(true);
         indi.SetActive(false);
         Time.timeScale = 1f;
