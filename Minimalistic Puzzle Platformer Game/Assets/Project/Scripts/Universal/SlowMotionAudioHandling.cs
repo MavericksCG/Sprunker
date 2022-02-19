dbfdc77d864c4c790f302f79b8e41b5d91ba1930
TheMaverick <89474607+MavericksCG@users.noreply.github.com>
2022-02-19 17:35:01 +0530

    Created SlowMotionAudioHandling
    - A universal script to handle all audio sources at once using 1 script. And it also doesn't have a gazillion fields!

--- Minimalistic Puzzle Platformer Game/Assets/Project/Scripts/Universal/SlowMotionAudioHandling.cs
+++ Minimalistic Puzzle Platformer Game/Assets/Project/Scripts/Universal/SlowMotionAudioHandling.cs
@@ -1,35 +1,30 @@
+using System;
 using UnityEngine;
 
 public class SlowMotionAudioHandling : MonoBehaviour {
 
-    [Header("References & Variables")]
-    [SerializeField] private AudioSource jump;
-    [SerializeField] private AudioSource superJump;
-    [SerializeField] private AudioSource soundtrack;
-
-    [Range(0f, 1f)] [SerializeField] private float audioLerpSpeed;
-
-    [SerializeField] private float pitchInSlowMotion = 0.1f;
-    [SerializeField] private float normalPitch = 1f;
+    private AudioSource[] sources;
 
     private SlowMotion slowMotion;
 
+    [SerializeField] private float pitchInSlowMotion = 0.1f;
+    [Range(0f, 1f)] [SerializeField] private float audioLerpSpeed = 0.01f;
 
     private void Awake () {
+        // Get All Audio Source Components in the children.
+        sources = GetComponentsInChildren<AudioSource>();
+        // Get Slow Motion.
         slowMotion = FindObjectOfType<SlowMotion>();
     }
 
     private void Update () {
-        if (slowMotion.slowMotionActive) { 
-            jump.pitch = Mathf.Lerp(jump.pitch, pitchInSlowMotion, audioLerpSpeed);
-            superJump.pitch = Mathf.Lerp(superJump.pitch, pitchInSlowMotion, audioLerpSpeed);
-            soundtrack.pitch = Mathf.Lerp(soundtrack.pitch, pitchInSlowMotion, audioLerpSpeed);
-        }
-        else {
-            jump.pitch = Mathf.Lerp(jump.pitch, normalPitch, audioLerpSpeed);
-            superJump.pitch = Mathf.Lerp(superJump.pitch, normalPitch, audioLerpSpeed);
-            soundtrack.pitch = Mathf.Lerp(soundtrack.pitch, normalPitch, audioLerpSpeed);
+        foreach (AudioSource s in sources) {
+            if (slowMotion.slowMotionActive) {
+                s.pitch = Mathf.Lerp(s.pitch, pitchInSlowMotion, audioLerpSpeed);
+            }
+            else {
+                s.pitch = Mathf.Lerp(s.pitch, 1f, audioLerpSpeed);
+            }
         }
     }
-
 }
